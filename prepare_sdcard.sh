c03d51ffe858c11e487b8d105e16aebdf6313893
ting su <tsuletgo@gmail.com>
2015-09-16 21:19:10 +0800

    [add] env files

--- prepare_sdcard.sh
+++ prepare_sdcard.sh
@@ -1,12 +1,31 @@
 #! /bin/sh
 
 adb -s emulator-5554 push CV-Ting_Su.pdf /mnt/sdcard/
+adb -s emulator-5554 push facebook_work.pdf /mnt/sdcard/
 adb -s emulator-5554 push a00001.vcf /mnt/sdcard/
+adb -s emulator-5554 push 00001.vcf /mnt/sdcard/
+adb -s emulator-5554 push 00002.vcf /mnt/sdcard/
+adb -s emulator-5554 push 00003.vcf /mnt/sdcard/
 adb -s emulator-5554 push license.txt /mnt/sdcard/
-adb -s emulator-5554 push music.mp3 /mnt/sdcard/
-adb -s emulator-5554 push pic.jpg /mnt/sdcard/
+adb -s emulator-5554 push music1.mp3 /mnt/sdcard/
+adb -s emulator-5554 push music2.mp3 /mnt/sdcard/
+adb -s emulator-5554 push music.zip /mnt/sdcard/
+adb -s emulator-5554 push pic1.jpg /mnt/sdcard/
+adb -s emulator-5554 push pic2.jpg /mnt/sdcard/
+adb -s emulator-5554 push pic3.jpg /mnt/sdcard/
+adb -s emulator-5554 push pic4.jpg /mnt/sdcard/
 
 adb -s emulator-5556 push CV-Ting_Su.pdf /mnt/sdcard/
+adb -s emulator-5556 push facebook_work.pdf /mnt/sdcard/
 adb -s emulator-5556 push a00001.vcf /mnt/sdcard/
+adb -s emulator-5556 push 00001.vcf /mnt/sdcard/
+adb -s emulator-5556 push 00002.vcf /mnt/sdcard/
+adb -s emulator-5556 push 00003.vcf /mnt/sdcard/
 adb -s emulator-5556 push license.txt /mnt/sdcard/
-adb -s emulator-5556 push music.mp3 /mnt/sdcard/
+adb -s emulator-5556 push music1.mp3 /mnt/sdcard/
+adb -s emulator-5556 push music2.mp3 /mnt/sdcard/
+adb -s emulator-5556 push music.zip /mnt/sdcard/
+adb -s emulator-5556 push pic1.jpg /mnt/sdcard/
+adb -s emulator-5556 push pic2.jpg /mnt/sdcard/
+adb -s emulator-5556 push pic3.jpg /mnt/sdcard/
+adb -s emulator-5556 push pic4.jpg /mnt/sdcard/
